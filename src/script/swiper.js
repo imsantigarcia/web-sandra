cdc0c5e8feb870a78cd10d90c07f8f9a299c377b
Santiago García <hola@caminoconsantiago.com>
2025-07-11 14:35:43 +0200

    update

--- src/script/swiper.js
+++ src/script/swiper.js
@@ -1,17 +1,18 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,                     // permite que el carrusel sea infinito
-  spaceBetween: 30,              // separa 30px entre slides (aún así no se asoma si solo hay 1 visible)
-  slidesPerView: 1,              // SOLO una slide visible a la vez → no se asoma nada
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+    el: ".swiper-pagination",
+    clickable: true,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
   },
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false
-  }
 });
